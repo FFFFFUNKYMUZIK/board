efdee46c988e3e0cc311977a259b1002a5a5ce07
Jaehoon <pf7@naver.com>
2021-06-13 18:06:47 +0900

    Implement write reply, move config folder under the WEB-INF folder

--- src/main/java/com/board/controller/ReplyController.java
+++ src/main/java/com/board/controller/ReplyController.java
@@ -4,15 +4,24 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.board.service.ReplyService;
+import com.board.domain.ReplyVO;
 
 @Controller
 @RequestMapping("/reply/*")
 public class ReplyController {
 
 	@Inject
-	ReplyService service;
-
+	ReplyService replyService;
+	
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String postWrite(ReplyVO vo) throws Exception {
+			
+		replyService.write(vo);
+		
+		return "redirect:/board/view?bno=" + vo.getBno();
+	}
 	
 }
